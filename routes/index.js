23789862fa1f68837e96a05f4a2537ac06c1a1e3
alexc957 <alexcoronel1995@gmail.com>
2020-12-08 14:28:46 -0500

    added index/users routes

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,43 @@ router.get('/', (req, res, next) => {
 });
 
 
+router.get('/register',(req,res,next)=>{
+  res.send('GET /REGISTER')
+})
 
+router.post('/register',(req,res,next)=>{
+  res.send('post /REGISTER')
+})
+
+router.get('/login',(req,res,next)=>{
+  res.send('get /login')
+})
+
+router.post('/login',(req,res,next)=> {
+  res.send('post /login')
+})
+
+
+router.get('/profile',(req,res,next)=>{
+  res.send('get /profile')
+})
+
+
+
+router.put('/profile/:user_id',(req,res,next)=>{
+  res.send('PUT /profile/:user_id')
+})
+
+router.get('/forgot-pw',(req,res,next)=>{
+  res.send('get /forgot-pw')
+})
+
+router.put('/forgot-pw',(req,res,next)=>{
+  res.send('put /forgot-pw')
+})
+
+router.put('/reset-pw/:token',(req,res,next)=>{
+  res.send('get /rest-pw/token')
+})
 
 module.exports = router;
